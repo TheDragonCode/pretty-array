2a55e54f1763d5d62c6aa633fee4e463b86a7ff4
Andrey Helldar <helldar@ai-rus.com>
2020-12-22 14:09:29 +0300

    Revert stub

--- tests/stubs/source.php
+++ tests/stubs/source.php
@@ -1,19 +1,19 @@
 <?php
 
-return [
-    'foo'     => 1,
-    'bar'     => 2,
-    'baz'     => 3,
+return array (
+    'foo' => 1,
+    'bar' => 2,
+    'baz' => 3,
     'qwe rty' => 'qaz',
-    'baq'     => [
-        0     => 'qwe',
-        '1'   => 'rty',
+    'baq' => array (
+        0 => 'qwe',
+        '1' => 'rty',
         'asd' => 'zxc',
-    ],
-    'asd fgh' => [
+    ),
+    'asd fgh' => array (
         'foo bar baz' => 'qwe',
-        2             => 'rty',
-        'qaw sed'     => 'zxc',
-    ],
+        2 => 'rty',
+        'qaw sed' => 'zxc',
+    ),
     2 => 'iop',
-];
+);
