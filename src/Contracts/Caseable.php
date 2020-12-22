ecc2c27e266ee3f0ac1aaa8c816749091916c399
Andrey Helldar <andrey-helldar@users.noreply.github.com>
2020-12-22 11:07:48 +0000

    Apply fixes from StyleCI

--- src/Contracts/Caseable.php
+++ src/Contracts/Caseable.php
@@ -4,15 +4,15 @@ namespace Helldar\PrettyArray\Contracts;
 
 interface Caseable
 {
-    const CAMEL_CASE = 1;
+    public const CAMEL_CASE = 1;
 
-    const KEBAB_CASE = 3;
+    public const KEBAB_CASE = 3;
 
-    const NO_CASE = 0;
+    public const NO_CASE = 0;
 
-    const PASCAL_CASE = 4;
+    public const PASCAL_CASE = 4;
 
-    const SNAKE_CASE = 2;
+    public const SNAKE_CASE = 2;
 
     public function setCase(int $type = self::NO_CASE): void;
 }
